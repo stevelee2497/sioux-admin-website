0547007ac681cb606aa704571eaffe8273db046b
Quoc Tran <quoc.tran@sioux.asia>
2019-12-15 11:03:24 +0700

    fix wrong destination

--- publish.sh
+++ publish.sh
@@ -1,5 +1,4 @@
 CURRENT_PATH=$(pwd)
-OUTPUT_PATH="${CURRENT_PATH}/build-api"
 
 echo "Cleaning ..."
 rm -rf dist
@@ -8,7 +7,5 @@ echo "Building web packages ..."
 yarn install && yarn umi build
 
 echo "Send packages to server"
-scp -C -r dist ubuntu@192.168.236.128:~/sioux-admin-web
-ssh ubuntu@192.168.236.128 "cd ~/sioux-admin-web;docker-compose up -d --build sioux-admin-web"
-
-cd "${CURRENT_PATH}"
+scp -C -r dist ubuntu@192.168.236.128:~/sioux-admin-website
+ssh ubuntu@192.168.236.128 "cd ~/sioux-admin-website;docker-compose up -d --build sioux-admin-website"

18cf292b21e7d0127c446241d7a434397adcfe10
Quoc Tran <quoc.tran@sioux.asia>
2019-12-15 17:29:54 +0700

    update new server

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@ echo "Building web packages ..."
 yarn install && yarn umi build
 
 echo "Send packages to server"
-scp -C -r dist ubuntu@192.168.236.128:~/sioux-admin-website
-ssh ubuntu@192.168.236.128 "cd ~/sioux-admin-website;docker-compose up -d --build sioux-admin-website"
+scp -C -r dist ssh ubuntu@52.187.169.33:~/sioux-admin-website
+ssh ubuntu@52.187.169.33 "cd ~/sioux-admin-website;docker-compose up -d --build sioux-admin-website"

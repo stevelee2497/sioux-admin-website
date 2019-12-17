cbee61606f3dfd601168b6482709968ed598f08d
Quoc Tran <Quoc.Tran@sioux.asia>
2019-12-17 13:51:47 +0700

    delete console log

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -8,10 +8,8 @@ import { parseImage } from '../../utils/images';
 class Activities extends Component {
   render() {
     const { employees, task: { taskActions } } = this.props;
-    console.log(employees, taskActions);
     return _.map(taskActions, action => {
       const member = employees[action.userId];
-      console.log(member);
       return (
         <div key={action.id} style={{ display: 'flex', marginRight: 25, marginTop: 10, alignItems: 'center' }}>
           <Avatar style={{ marginTop: 10 }} src={parseImage(member.avatarUrl)} />
